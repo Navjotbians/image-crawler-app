ae6777b99a71d20b1aa0a2900eaa4dabbec00c07
Navjot Bians <Navjotbians1988@gmail.com>
2021-04-28 15:44:36 -0400

    fetch 1st level links

--- notebooks/crawler_basic.py
+++ notebooks/crawler_basic.py
@@ -3,86 +3,21 @@
 
 # # IMAGE CRAWLER
 
-# In[26]:
+# In[17]:
 
 
 import requests as req
 from bs4 import *
 
 
-# In[27]:
+# In[22]:
 
 
 url = "https://www.airbnb.co.uk/s/Ljubljana--Slovenia/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&query=Ljubljana%2C%20Slovenia&place_id=ChIJ0YaYlvUxZUcRIOw_ghz4AAQ&checkin=2020-11-01&checkout=2020-11-08&source=structured_search_input_header&search_type=autocomplete_click"
+url2 = "http://4chan.org/"
 
 
-# In[28]:
-
-
-## req will send the request to this link and get the data in text format from this link webpage
-request = req.get(url)
-
-
-# In[29]:
-
-
-## we have this text format of webpage from req.get now we want to parse it in html format. 
-## once we get the html format then only we could get the image links
-soup = BeautifulSoup(request.text, "html.parser") ## this will change text into html format
-hrefs = soup.find_all(href=True)
-images = soup.find_all('img')
-
-
-# In[30]:
-
-
-## grab link from all_img_links and append into links list
-links = []
-for img_link in images:
-    links.append(img_link['src'])
-links
-
-
-# In[51]:
-
-
-## grabbing all the links including the image links
-for href in hrefs:
-    print(href["href"])
-
-
-# In[60]:
-
-
-## function to get img links
-def parse_url(url):
-    request = req.get(url)
-    soup = BeautifulSoup(request.text, "html.parser")
-    hrefs = soup.find_all(href=True)
-    images = soup.find_all('img')
-    links = [link["src"] for link in images]
-    return links, hrefs
-    
-
-
-# In[65]:
-
-
-links, hrefs = parse_url("http://4chan.org/")
-links
-
-
-# In[66]:
-
-
-href_links = []
-for href in hrefs:
-    prepare_url(href["href"])
-    href_links.append(href["href"])
-href_links
-
-
-# In[67]:
+# In[23]:
 
 
 def prepare_url(url):
@@ -94,10 +29,37 @@ def prepare_url(url):
     return url
 
 
-# In[68]:
+# In[28]:
 
 
-prepare_url('https://a0.muscache.com/airbnb/static/icons/apple-touch-icon-76x76-3b313d93b1b5823293524b9764352ac9.png')
+## function to get img links
+def parse_url(url):
+    ## req will send the request to this link and get the data in text format from this link webpage
+    request = req.get(url)
+    ## this will change text into html format
+    soup = BeautifulSoup(request.text, "html.parser")
+    hrefs = soup.find_all(href=True)
+    ## grab all img links and append into links list
+    images = soup.find_all('img')
+    links = [prepare_url(link["src"]) for link in images]
+    return links, hrefs
+    
+
+
+# In[29]:
+
+
+links, hrefs = parse_url(url2)
+links
+
+
+# In[30]:
+
+
+href_links = []
+for href in hrefs:
+    href_links.append(prepare_url(href["href"]))
+href_links
 
 
 # In[ ]:
