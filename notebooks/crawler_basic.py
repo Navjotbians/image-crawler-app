13ff1cf5ba407550fefe9dea9f8b98aa19c43f4b
Navjot Bians <Navjotbians1988@gmail.com>
2021-05-01 11:46:28 -0400

    clean repo

--- notebooks/crawler_basic.py
+++ notebooks/crawler_basic.py
@@ -3,6 +3,18 @@
 
 # # IMAGE CRAWLER
 
+# 1. An application must take the form of a Web API server.
+# 2. We expect that the app will take a list of URLs as input and a number of threads,
+# and return a job ID used to fetch crawling information/results.
+# 3. The app should be able to run multiple crawling jobs at the same time.
+# 4. We expect to be able to get information/results about a running/finished job
+# from your application (using its job ID).
+# 5. The data extracted will be a list of image URLs (gif, jpg, png) as output.
+# 6. The app should crawl the URLs recursively only until the second level
+# (to avoid a large amount of data): Fetch the images for each given URL and their children.
+# 7. By default, the app will crawl using one thread/coroutine, however,
+# we should be able to specify the number of thread/coroutine to use when creating a new job.
+
 # In[25]:
 
 
